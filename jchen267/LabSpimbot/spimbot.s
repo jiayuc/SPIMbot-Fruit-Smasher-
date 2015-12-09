fc2d4d6e254397fb66896b6f4dd6645e58c63c33
jchen267 <jchen267@c4e75f5d-2d13-4d0c-9813-c6b6593af95e>
2015-12-09 01:22:30 +0000

    sub

--- jchen267/LabSpimbot/spimbot.s
+++ jchen267/LabSpimbot/spimbot.s
@@ -58,7 +58,7 @@ directions:
 
 .align 2
 fruit_data: .space 260
-num_smooshed: .space 4
+# num_smooshed: .space 4
 
 .align 2
 puzzle_space:  .space 8192
@@ -83,9 +83,9 @@ node_memory: .space 4096
 main:
 	li  $t6, 1  #flag is 0 when solving puzzle, once 1 we should request new puzzle
 	li  $s6, 0
-	sw  $s6, num_smooshed 
+	# sw  $s6, num_smooshed 
 	# enable interrupts
-	li	$t4, TIMER_MASK		# timer interrupt enable bit
+	# li	$t4, TIMER_MASK		# timer interrupt enable bit
 	or  $t4, SMOOSHED_MASK  # added, enable fruit_smooshed interrupt 
 
 	# or  $t4, $t4, ENERGY_OUT_MASK
@@ -95,21 +95,21 @@ main:
 	mtc0	$t4, $12		# set interrupt mask (Status register)
 
 # go to bottom mid of screen
-go_down:
-	li 	$s0, 90
-	sw  $s0, ANGLE
-	li  $s0, 4
-	sw	$s0, VELOCITY
-	li  $s0, 1
-	sw	$s0, ANGLE_CONTROL
+# go_down:
+# 	li 	$s0, 90
+# 	sw  $s0, ANGLE
+# 	li  $s0, 1
+# 	sw	$s0, ANGLE_CONTROL
+# 	li  $s0, 4
+# 	sw	$s0, VELOCITY
 
-# get the y coordinate
-keep_walking:
-    lw  $s2, BOT_Y
-    li  $s3, 280
-    ble $s2, $s3, keep_walking
-   
-    j   chase_fruit
+# # get the y coordinate
+# keep_walking:
+#     lw  $s2, BOT_Y
+#     li  $s3, 200
+#     ble $s2, $s3, keep_walking
+
+#     j   chase_fruit
 
 regenerate:
 # step 2: load the address of this memory into register
@@ -120,7 +120,11 @@ regenerate:
 	j   chase_fruit_cont
 
 chase_fruit:
+	lw  $t1, GET_ENERGY
+	bgt $t1, 100, chase_fruit_cont
 	beq $t6, 1, regenerate # when t6 is 1, request new puzzle!
+
+	
 chase_fruit_cont:
 # step 2: load the address of this memory into register
 	la  $t0, fruit_data
@@ -128,73 +132,94 @@ chase_fruit_cont:
 	sw  $t0, FRUIT_SCAN
     lw  $s2, BOT_Y
 # check see if num_smooshed > 5, time to smash fruit?
-	lw  $s0, num_smooshed
-	bge $s0, 5, smash_fruit
+	# lw  $s0, num_smooshed
+	bge $s6, 5, smash_fruit
 
-	lw  $s4, 0($t0) # fruit id
-    beq $s4, $0, chase_fruit
+	# lw  $s4, 0($t0) # fruit id
+ #    beq $s4, $0, chase_fruit
+
+     
+    lw  $s1  BOT_Y
+
+    lw	$t1	 4($t0) #1_fruit_points
+    ble $s1  $t1 second
 	lw  $s5, 8($t0) # fruit_x
-    lw  $s1, BOT_X
-    #if at risk of out of bound x-wise, just scan the next fruit
-    bgt $s5, 280, chase_fruit
-    blt $s5, 20, chase_fruit
+	j gogogo
+second:
+    lw	$t1	 20($t0) #2_fruit_points
+    ble $s1  $t1 third
+	lw  $s5, 24($t0) # fruit_x
+	j gogogo
+third:
+    lw	$t1	 36($t0) #3_fruit_points
+    ble $s1  $t1 chase_fruit
+	lw  $s5, 40($t0) # fruit_x
+	j gogogo
+fourth:
+    lw	$t1	 52($t0) #4_fruit_points
+    ble $s1  $t1 fifth
+	lw  $s5, 56($t0) # fruit_x
+	j gogogo
+fifth:
+	lw  $t1, 68($t0) #5_fruit_points
+    ble $s1  $t1 chase_fruit
+	j gogogo
 
+
+gogogo:
+    lw  $s1, BOT_X
     bgt $s5, $s1, turn_to_right # fruit at right
     blt $s5, $s1, turn_to_left
     j   chase_fruit 
 
-smash_fruit: 
-# hit the bottom 
-	li 	$s0, 90
-	sw  $s0, ANGLE
-	li  $s0, 10
-	sw	$s0, VELOCITY
-	li  $s0, 1
-	sw	$s0, ANGLE_CONTROL
-
-keep_walking_till_bonk:
-    lw  $s0, num_smooshed
-	bge $s0, 5, keep_walking_till_bonk
-    # walk back to normal routine to catch fruit
-go_up:
-	li 	$s0, 270
-	sw  $s0, ANGLE
-	li  $s0, 10
-	sw	$s0, VELOCITY
-	li  $s0, 1
-	sw	$s0, ANGLE_CONTROL
-# get the y coordinate
-keep_walking_up:
-    lw  $s2, BOT_Y
-    li  $s3, 295
-    bge $s2, $s3, keep_walking
-    j   chase_fruit
-
-
 turn_to_left:
 	li 	$s0, 180
 	sw  $s0, ANGLE
-	li  $s0, 10
-	sw	$s0, VELOCITY
 	li  $s0, 1
 	sw	$s0, ANGLE_CONTROL	
+	li  $s0, 10
+	sw	$s0, VELOCITY
 
-keep_walking2:
-    lw  $s1, BOT_X
-    blt $s1, $s5, keep_walking2
     j   chase_fruit
 
 turn_to_right:    
 	li 	$s0, 0
 	sw  $s0, ANGLE
-	li  $s0, 10
-	sw	$s0, VELOCITY
 	li  $s0, 1
 	sw	$s0, ANGLE_CONTROL	
+	li  $s0, 10
+	sw	$s0, VELOCITY
 
-keep_walking3:
-    lw  $s1, BOT_X
-    bgt $s1, $s5, keep_walking3
+    j   chase_fruit
+
+smash_fruit: 
+# hit the bottom 
+	li 	$s0, 90
+	sw  $s0, ANGLE
+	li  $s0, 1
+	sw	$s0, ANGLE_CONTROL
+	li  $s0, 10
+	sw	$s0, VELOCITY
+
+keep_walking_till_bonk:
+    # lw  $s0, num_smooshed
+	bge $s6, 5, keep_walking_till_bonk
+    # walk back to normal routine to catch fruit
+go_up:
+	li 	$s0, 270
+	sw  $s0, ANGLE
+	li  $s0, 1
+	sw	$s0, ANGLE_CONTROL
+	li  $s0, 10
+	sw	$s0, VELOCITY
+
+# get the y coordinate
+    li  $s3, 270
+keep_walking_up:
+    lw  $s2, BOT_Y
+    bge $s2, $s3, keep_walking_up
+   	li  $s0, 0
+	sw	$s0, VELOCITY
     j   chase_fruit
 
 
@@ -228,8 +253,8 @@ interrupt_dispatch:			# Interrupt:
 	and	$a0, $k0, SMOOSHED_MASK	# is there a fruit_smooshed interrupt?
 	bne	$a0, 0, smooshed_interrupt
 
-	and	$a0, $k0, TIMER_MASK	# is there a timer interrupt?
-	bne	$a0, 0, timer_interrupt
+	# and	$a0, $k0, TIMER_MASK	# is there a timer interrupt?
+	# bne	$a0, 0, timer_interrupt
 
 	and	$a0, $k0, REQUEST_MASK	# is there a request_puzzle interrupt?
 	bne	$a0, 0, request_interrupt
@@ -246,20 +271,34 @@ interrupt_dispatch:			# Interrupt:
 
 smooshed_interrupt:
     sw	$a1, SMOOSHED_ACK	
-	lw  $s6, num_smooshed
+	# lw  $s6, num_smooshed
     add $s6, $s6, 1
 	# sw	$zero, VELOCITY		# ???s
-    sw  $s6, num_smooshed
+    # sw  $s6, num_smooshed
 
  	j	interrupt_dispatch	# see if other interrupts are waiting  
 
 bonk_interrupt:
-	sw  $s6, num_smooshed
+
+# 	li 	$s0, 90
+# 	sw  $s0, ANGLE
+# 	li  $s0, 1
+# 	sw	$s0, ANGLE_CONTROL
+# 	li  $s0, 4
+# 	sw	$s0, VELOCITY
+# wait:
+# 	lw  $a0 BOT_Y
+# 	ble $a0 293 wait
+
+	
+	# sw  $s6, num_smooshed
 	beq $s6, $0, acknowledge_bonk
+	sw $zero VELOCITY
 # num_smooshed is not 0, keep smashing
+smash_in_bonk:
 	sw  $s6, FRUIT_SMASH
 	add $s6, $s6, -1
-	j   bonk_interrupt
+	bne $s6, $0, smash_in_bonk
 
 acknowledge_bonk:
 	sw	$a1, BONK_ACK		# acknowledge interrupt
@@ -269,9 +308,9 @@ acknowledge_bonk:
 timer_interrupt:
 	sw	$a1, TIMER_ACK		# acknowledge interrupt
 
-	li	$t0, 90			# ???
-	sw	$t0, ANGLE		# ???
-	sw	$zero, ANGLE_CONTROL	# ???
+	# li	$t0, 90			# ???
+	# sw	$t0, ANGLE		# ???
+	# sw	$zero, ANGLE_CONTROL	# ???
 
 	lw	$v0, TIMER		# current time
 	add	$v0, $v0, 50000  
@@ -303,7 +342,7 @@ request_interrupt:
 	li	$v0, 0			# Set $v0 to 0 to confirm actually returned non-zero
 	li	$a2, 1
 	li	$a3, 0
-	jal	search_neighbors ##char *puzzle, const char *word, int row, int col)
+	jal	solve_puzzle ##char *puzzle, const char *word, int row, int col)
 	sw  $v0, SUBMIT_SOLUTION
 
 	#change new_node_address's value to node_memory's address
@@ -329,6 +368,104 @@ done:
 	move	$at, $k1		# Restore $at
 .set at 
 	eret
+###solve_puzzle##########################################
+
+solve_puzzle:
+	# Your code goes here :)
+	sub		$sp,$sp,36	#allocate stack memory
+	sw		$ra,0($sp)	
+	sw		$s0,4($sp)	#exist
+	sw		$s1,8($sp) 	#*puzzle
+	sw		$s2,12($sp)	#word
+	sw		$s3,16($sp)	#row	
+	sw		$s4,20($sp)	#num_row
+	sw		$s5,24($sp)	#col
+	sw		$s6,28($sp)	#num_cols
+	sw		$s7,32($sp)	#current_char
+	
+	
+	move 	$s1,$a0		#puzzle
+	move	$s2,$a1		#word
+	li		$s3,0		#row
+	lw		$s4,num_rows	#load num_row
+	
+	lw		$s6,num_cols	#load num_col
+	
+	lb		$t0,0($s2)	#word[0]
+
+puzzle_firstif:
+	bne		$t0,$0,not_if	#branch if !=0
+	j		return_true	#return ra
+
+not_if:
+	bge		$s3,$s4,return_false #branch if!(row<num_rows)
+	li		$s5,0		#col
+	j		second_for
+
+second_for:
+	bge		$s5,$s6,not_secondfor	#branch if!(col<col_rows)
+	move	$a0,$s1 	#puzzle
+	move	$a1,$s3		#row 
+	move	$a2,$s5		#col
+	jal		get_char
+	move	$s7,$v0		#current_char=get_char
+	lb		$t1,0($s2)		#target_char=word[0]
+	beq		$s7,$t1,second_if		#branch if current_char=target_char
+	add		$s5,$s5,1	#col++
+	j		second_for
+not_secondfor:
+	add		$s3,$s3,1	#row++
+	j		not_if
+	
+	
+second_if:
+	move	$a0,$s1		#set_char(puzzle, row, col, '*');
+	move	$a1,$s3
+	move	$a2,$s5
+	li		$a3,'*'
+	jal		set_char
+	
+	move	$a0,$s1		#search_neighbors(puzzle, word + 1, row, col);
+	add		$a1,$s2,1	#word+1
+	move	$a2,$s3
+	move	$a3,$s5
+	jal		search_neighbors
+	move	$s0,$v0		
+	
+	move	$a0,$s1				#set_char(puzzle, row, col, word[0]);
+	move	$a1,$s3
+	move	$a2,$s5
+	move	$a3,$s2	#word[0]
+	jal		set_char
+	
+	bne		$s0,$0,return_true 	#branch if exist==1
+	add		$s5,$s5,1	#col++
+	j		second_for
+	
+return_false:
+	li	$v0,0
+	j	puzzle_done
+
+return_true:
+	move	$a0,$s3		
+	move	$a1,$s5
+	move	$a2,$s0 
+	jal		set_node						 # return set_node(row, col, node1);
+	j	puzzle_done
+
+puzzle_done:
+	lw		$ra,0($sp)	
+	lw		$s0,4($sp)	#exist
+	lw		$s1,8($sp) 	#*puzzle
+	lw		$s2,12($sp)	#word
+	lw		$s3,16($sp)	#row	
+	lw		$s4,20($sp)	#num_row
+	lw		$s5,24($sp)	#col
+	lw		$s6,28($sp)	#num_cols
+	lw		$s7,32($sp)	#current_char
+	add		$sp,$sp,36
+	jr		$ra
+
 
 
 #search_neighbors--------------------------
@@ -394,8 +531,6 @@ sn_loop:
 	add	$s5, $s2, $t1		# next_row
 	lw	$t1, directions+4($t0)	# directions[i][1]
 	add	$s6, $s3, $t1		# next_col
-
-
 
 
 ##    if(next_row == num_rows)
@@ -502,6 +637,42 @@ sn_return:
 #     node->next = next;
 #     return node;
 # }
+
+
+# Gets char from a 2D array
+# Arguments:
+#	$a0: pointer to beginning of 2D array
+#	$a1: row
+#	$a2: col
+# Returns: char at that location
+.globl get_char
+get_char:
+	lw	$v0, num_cols
+	mul	$v0, $a1, $v0	# row * num_cols
+	add	$v0, $v0, $a2	# row * num_cols + col
+	add	$v0, $a0, $v0	# &array[row * num_cols + col]
+	lb	$v0, 0($v0)	# array[row * num_cols + col]
+	jr	$ra
+
+# Sets a char in a 2D array
+# Arguments:
+#	$a0: pointer to beginning of 2D array
+#	$a1: row
+#	$a2: col
+#	$a3: char to store into array
+# Returns: nothing
+.globl set_char
+set_char:
+	lw	$v0, num_cols
+	mul	$v0, $a1, $v0	# row * num_cols
+	add	$v0, $v0, $a2	# row * num_cols + col
+	add	$v0, $a0, $v0	# &array[row * num_cols + col]
+	sb	$a3, 0($v0)	# array[row * num_cols + col] = c
+	jr	$ra
+
+
+
+
 
 
 
